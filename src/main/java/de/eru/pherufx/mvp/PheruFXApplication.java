1939fe014f61e87a1a5ea40fe1b34bc8aaaa6fbb
pheru <Philipp.Bruckner@freenet.de>
2015-03-30 21:33:14 +0200

    #9: Eventhandler für start()

--- src/main/java/de/eru/pherufx/mvp/PheruFXApplication.java
+++ src/main/java/de/eru/pherufx/mvp/PheruFXApplication.java
@@ -1,6 +1,7 @@
 package de.eru.pherufx.mvp;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javax.enterprise.util.AnnotationLiteral;
 import org.jboss.weld.environment.se.Weld;
@@ -12,12 +13,39 @@ import org.jboss.weld.environment.se.WeldContainer;
  */
 public class PheruFXApplication extends Application {
 
+    private static EventHandler<StartEvent> startingHandler;
+    private static EventHandler<StartEvent> startFinishedHandler;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        StartEvent startEvent = new StartEvent();
+        startEvent.setPrimaryStage(primaryStage);
+        if (startingHandler != null) {
+            startingHandler.handle(startEvent);
+        }
         WeldContainer weldContainer = new Weld().initialize();
         weldContainer.instance().select(ApplicationParametersProvider.class).get().setParameters(getParameters());
         weldContainer.event().select(Stage.class, new AnnotationLiteral<StartApplication>() {
         }).fire(primaryStage);
+        if (startFinishedHandler != null) {
+            startFinishedHandler.handle(startEvent);
+        }
+    }
+
+    public static EventHandler<StartEvent> getOnStarting() {
+        return startingHandler;
+    }
+
+    public static void setOnStarting(EventHandler<StartEvent> initializeHandler) {
+        PheruFXApplication.startingHandler = initializeHandler;
+    }
+
+    public static EventHandler<StartEvent> getOnStartFinished() {
+        return startFinishedHandler;
+    }
+
+    public static void setOnStartFinished(EventHandler<StartEvent> startFinishedHandler) {
+        PheruFXApplication.startFinishedHandler = startFinishedHandler;
     }
 
 }
