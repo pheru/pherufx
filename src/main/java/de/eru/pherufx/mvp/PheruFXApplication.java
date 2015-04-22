69618deec953d708dabfaabdf4192d875f08a57b
pheru <philipp.bruckner@freenet.de>
2015-04-22 21:07:11 +0200

    S: protected getter für weldContainer

--- src/main/java/de/eru/pherufx/mvp/PheruFXApplication.java
+++ src/main/java/de/eru/pherufx/mvp/PheruFXApplication.java
@@ -3,10 +3,10 @@ package de.eru.pherufx.mvp;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
+
 import javax.enterprise.util.AnnotationLiteral;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
-
 /**
  *
  * @author Philipp Bruckner
@@ -15,6 +15,7 @@ public class PheruFXApplication extends Application {
 
     private static EventHandler<StartEvent> startingHandler;
     private static EventHandler<StartEvent> startFinishedHandler;
+    private static WeldContainer weldContainer;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -23,13 +24,17 @@ public class PheruFXApplication extends Application {
         if (startingHandler != null) {
             startingHandler.handle(startEvent);
         }
-        WeldContainer weldContainer = new Weld().initialize();
+        weldContainer = new Weld().initialize();
         weldContainer.instance().select(ApplicationParametersProvider.class).get().setParameters(getParameters());
         weldContainer.event().select(Stage.class, new AnnotationLiteral<StartApplication>() {
         }).fire(primaryStage);
         if (startFinishedHandler != null) {
             startFinishedHandler.handle(startEvent);
         }
+    }
+    
+    protected static WeldContainer getWeldContainer(){
+        return weldContainer;
     }
 
     public static EventHandler<StartEvent> getOnStarting() {
