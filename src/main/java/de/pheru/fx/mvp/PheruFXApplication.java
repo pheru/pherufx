d0459637682916de7647a62274d35f4eaf5021a5
pheru <philipp.bruckner@freenet.de>
2016-01-08 11:00:22 +0100

    S: start-Ablauf angepasst

--- src/main/java/de/pheru/fx/mvp/PheruFXApplication.java
+++ src/main/java/de/pheru/fx/mvp/PheruFXApplication.java
@@ -3,13 +3,12 @@ package de.pheru.fx.mvp;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
-
-import javax.enterprise.util.AnnotationLiteral;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
+import javax.enterprise.util.AnnotationLiteral;
+
 /**
- *
  * @author Philipp Bruckner
  */
 public class PheruFXApplication extends Application {
@@ -19,25 +18,44 @@ public class PheruFXApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         beforeStart();
-
-        //Damit wird sichergestellt, dass auch alle JavaFX-Aktivitäten in beforeStart() abgeschlossen sind
-        Platform.runLater(() -> {
+        new Thread(() -> {
             weldContainer = new Weld().initialize();
             weldContainer.instance().select(ApplicationParametersProvider.class).get().setParameters(getParameters());
             weldContainer.instance().select(PrimaryStageProvider.class).get().setPrimaryStage(primaryStage);
-            weldContainer.event().select(Stage.class, new AnnotationLiteral<StartApplication>() {
+            load();
+            Platform.runLater(() -> {
+                weldContainer.event().select(Stage.class, new AnnotationLiteral<StartApplication>() {
                 }).fire(primaryStage);
-
-            startFinished();
-        });
+                afterStart();
+            });
+        }).start();
     }
 
+    /**
+     * This method is called immediately at the beginning of the {@link #start(Stage)} method.
+     * The default implementation does nothing.<br/>
+     * This method is called on the JavaFX Application Thread.
+     */
     public void beforeStart() {
-        //empty
+        //empty by default
     }
 
-    public void startFinished() {
-        //empty
+    /**
+     * This method is called after the weld-container is initialized.
+     * The default implementation does nothing.<br/>
+     * This method is NOT called on the JavaFX Application Thread.
+     */
+    public void load() {
+        //empty by default
+    }
+
+    /**
+     * This method is called after the {@link StartApplication} event is fired.
+     * The default implementation does nothing.<br/>
+     * This method is called on the JavaFX Application Thread.
+     */
+    public void afterStart() {
+        //empty by default
     }
 
     protected static WeldContainer getWeldContainer() {
